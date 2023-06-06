d7a519c5bc4b4762fa1ad7ede7486df9ff583b46
Sergio <sergioa.orozcoo@gmail.com>
2023-06-06 17:28:11 -0500

    Add Validations and modal per contract

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,10 @@ export default {
       clientInfo: false,
       legalInfo : null,
       isLoading: false,
+      historical: {},
       filterInfo: '',
-      range1: 'statusInformation!A:E',
-      range2: 'Sheet2!A:E',
+      range1: 'statusInformation!A:H',
+      range2: 'Sheet2!A:H',
       errorText : '',
     }
   },
@@ -31,10 +32,14 @@ export default {
         this.clientInfo = true
         const data = response.data.valueRanges
         let mergedInformation = data.reduce((acc,sheet) => acc.concat(sheet.values), []);
+        const uniqueContracts = new Set();
         const formattedData = mergedInformation.map(row => Object.fromEntries(mergedInformation[0].map((key,index) => [key, row[index]] )))
-        this.legalInfo = formattedData.filter(item => item.user_id == this.filterInfo)
+        this.legalInfo = formattedData.filter(item => item.CEDULA == this.filterInfo &&
+        uniqueContracts.has(item.CONTRATO) === false && 
+        uniqueContracts.add(item.CONTRATO))
+        this.historical = formattedData
         this.isLoading = false
-        return (!this.legalInfo.length) ? this.errorText = 'Este usuario no tiene procesos activos.' : this.errorText = ''
+        return (!this.legalInfo.length || null) ? this.errorText = 'Este usuario no tiene procesos activos.' : this.errorText = ''
       })
       .catch(error => {
         console.error(error)
@@ -47,7 +52,7 @@ export default {
 
 <template>
   <div class="centeredContent flex flex-col items-center lg:pt-24 pt-6">
-    <img src="../public/LogoMiDefensa.png" alt="logo Mi Defensa Abogados">
+    <img src="../LogoMiDefensa.png" alt="logo Mi Defensa Abogados">
     <LegalSearch msg="Mi Defensa Abogados"/>
     <figure class="mt-10 mb-10 lg:w-2/4 w-full relative flex items-center">
       <input @change="validateUser()" v-model="filterInfo" placeholder="Digita tu cédula sin puntos..." class="shadow-sm py-4 px-2 rounded-lg w-full pl-12 border-cyan-900 border" type="number" name="cedula" id="">
@@ -55,7 +60,7 @@ export default {
     </figure>
     <ProgressSpinner class="w-10 h-10" v-if="isLoading" strokeWidth="3" animationDuration="2s"></ProgressSpinner>
     <section class="mt-1 w-11/12 lg:w-auto" v-if="clientInfo" v-motion-slide-bottom>
-      <cardInfo v-for="card in legalInfo" class="my-6" :userObj="card"/>
+      <cardInfo v-for="card in legalInfo" class="my-6" :infoModal="this.historical"  :userObj="card"/>
       <p v-if="errorText" class="px-5 py-1 bg-red-100 lg:text-xl text-md text-red-950 mix-blend-multiply rounded-full text-center">{{errorText}}</p>
     </section>
   </div>
--- src/components/cardInfo.vue
+++ src/components/cardInfo.vue
@@ -1,15 +1,33 @@
 <script>
+import Dialog from 'primevue/dialog';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 export default {
+  data(){
+    return {
+      visible: false,
+      modalData: null
+    }
+  },
   props: {
     userObj: null,
+    infoModal: null
   },
   methods: {
-
+    checkModal() {
+      this.modalData = this.infoModal.filter(item => item.CONTRATO == this.userObj.CONTRATO);
+      this.visible = true;
+    }
+  },
+  components: {
+    Dialog,
+    DataTable,
+    Column
   },
   computed: {
     checkStatus() {
-      return (this.userObj.status.toLowerCase() != 'finalizado') ? 'bg-red-100' : 'bg-lime-100';
-    }
+      return (this.userObj.ESTATUS.toLowerCase() != 'finalizado') ? 'bg-red-100' : 'bg-lime-100';
+    },
   }
 }
 
@@ -18,28 +36,52 @@ export default {
 <template>
   <article class="text-center py-8 lg:px-12 lg:h-28 rounded-lg lg:py-0 lg:gap-8 gap-4 flex flex-col lg:flex-row items-center justify-between bg-white shadow-md">
     <div class="flex gap-4">
-      <figure class="w-12 h-12 rounded-full bg-orange-200 flex place-items-center justify-center">
+      <figure class="hidden w-12 h-12 rounded-full bg-orange-200 lg:flex place-items-center justify-center">
         <i class="pi pi-user text-orange-900 text-3xl"></i>
       </figure>
-      <div class="text-left">
+      <div class="lg:text-left text-center px-7 lg:px-0">
         <p>Nombre de Usuario:</p>
-        <p class="font-semibold">{{userObj.user_name}}</p>
+        <p class="font-semibold">{{userObj.NOMBRE}}</p>
       </div>
     </div>
     <i class="divider h-0.5 w-full lg:w-0.5 lg:h-full bg-slate-100"></i> <!--Divider-->
     <div class="w-[180px]">
       <p>Tipo de Proceso:</p>
-      <p class="font-semibold">{{userObj.type}}</p>
+      <p class="font-semibold">{{userObj.PROCESO}}</p>
     </div>
     <i class="divider h-0.5 w-full lg:w-0.5 lg:h-full bg-slate-100"></i> <!--Divider-->
     <div>
       <p>Estado del proceso:</p>
-      <p :class="checkStatus" class="mt-1 py-1 px-2 font-semibold rounded-md text-center">{{userObj.status}}</p>
+      <p :class="checkStatus" class="mt-1 py-1 px-2 font-semibold rounded-md text-center">{{userObj.ESTATUS}}</p>
     </div>
     <i class="divider h-0.5 w-full lg:w-0.5 lg:h-full bg-slate-100"></i> <!--Divider-->
     <div>
-      <p>Última Actualización:</p>
-      <p class="font-semibold rounded-md text-center">{{userObj.date}}</p>
+      <p>Última Actuación:</p>
+      <p class="font-semibold rounded-md text-center">{{userObj.ACTUACION}}</p>
     </div>
+    <div>
+      <i class="divider h-0.5 w-full lg:w-0.5 lg:h-full bg-slate-100"></i> <!--Divider-->
+      <div v-if="userObj.ANEXOS">
+        <p>Anexos:</p>
+        <a class="font-semibold flex items-center hover:text-blue-700 text-blue-500 rounded-md text-center cursor-pointer" :href="userObj.ANEXOS" target="_blank"><i class="pi-link pi mr-1"></i> Link1</a>
+      </div>
+    </div>
+    <div class="contents">
+      <i class="divider h-0.5 w-full lg:w-0.5 lg:h-full bg-slate-100"></i> <!--Divider-->
+      <div>
+        <button class="text-blue-500 hover:text-blue-700" @click="checkModal">Ver Historial</button>
+      </div>
+    </div>
+    <Dialog v-model:visible="visible" modal header="Historial del proceso" class="py-5 w-4/5">
+      <div class="py-5">
+        <DataTable :value="modalData" class="sm">
+          <Column field="CONTRATO" header="Contrato"></Column>
+          <Column field="PROCESO" header="Proceso"></Column>
+          <Column field="ACTUACION" header="Fecha"></Column>
+          <Column field="ESTATUS" header="Estado"></Column>
+          <Column v-if="modalData.ANEXOS" field="ANEXOS" header="Anexos"></Column>
+        </DataTable>
+      </div>
+    </Dialog>
   </article>
 </template>
